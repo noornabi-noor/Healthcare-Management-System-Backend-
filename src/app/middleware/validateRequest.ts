6ab098d220e0c1ae82e766389deb7ca97b0f2f1e
noornabi-noor <noornabinoor1770@gmail.com>
2026-04-04 09:36:55 +0600

    Create Specialty API For Image Upload

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -3,6 +3,11 @@ import z, { ZodObject } from "zod";
 
 export const validateRequest = (zodSchema: z.ZodObject) => {
   return (req: Request, res: Response, next: NextFunction) => {
+
+    if(req.body.data){
+        req.body = JSON.parse(req.body.data);
+    }
+    
     const parseResult = zodSchema.safeParse(req.body);
 
     if (!parseResult.success) {
