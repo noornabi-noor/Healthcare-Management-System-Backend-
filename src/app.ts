aa72c80ca1c4bc6e87a30bc02d700819b0a6d8b7
noornabi-noor <noornabinoor1770@gmail.com>
2026-04-07 22:57:57 +0600

    Creating Auto-Cancelling With Cron Job

--- src/app.ts
+++ src/app.ts
@@ -10,29 +10,40 @@ import cors from "cors";
 import { envVars } from "./app/config/env";
 import qs from "qs";
 import { paymentController } from "./app/modules/payment/payment.controller";
+import cron from "node-cron";
+import { appointmentService } from "./app/modules/appointment/appointment.services";
 
-const app : Application = express();
-app.use(express.urlencoded({extended: true}));
+const app: Application = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.set("query parser", (str : string) => qs.parse(str));
+app.set("query parser", (str: string) => qs.parse(str));
 app.set("view engine", "ejs");
 app.set("views", path.resolve(process.cwd(), '/src/app/templates'));
 
 app.post("/webhook", express.raw({ type: "application/json" }), paymentController.handleStripeWebhookEvent)
 
 app.use(cors({
-    origin : [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
-    credentials : true,
-    methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders : ["Content-Type", "Authorization"]
+    origin: [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use("/api/auth", toNodeHandler(auth));
 
 app.use("/api/v1", indexRoutes);
 
-app.get("", (req : Request, res: Response)=>{
+cron.schedule("*/25 * * * *", async () => {
+    try {
+        console.log("Running cron job to cancel unpaid appointments...");
+        await appointmentService.cancelUnpaidAppointments();
+    } catch (error: any) {
+        console.error("Error occurred while canceling unpaid appointments:", error.message);
+    }
+});
+
+app.get("", (req: Request, res: Response) => {
     res.send("Hello world!");
 });
 
